06efc1460fa0b0cb48bdd24c0c79e046de250348
Matthew Benjamin <mbenjamin618@gmail.com>
2016-07-15 12:43:24 -0400

    Animations and Layout Update

--- 17 - charLimit/17 - charLimit/ViewController.swift
+++ 17 - charLimit/17 - charLimit/ViewController.swift
@@ -13,10 +13,12 @@ class ViewController: UIViewController, UITextViewDelegate {
     
     @IBOutlet weak var charCountLabel: UILabel!
     @IBOutlet weak var textView: UITextView!
+    var resetFont = false
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         textView.delegate = self
     }
 
@@ -28,10 +30,46 @@ class ViewController: UIViewController, UITextViewDelegate {
     func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
         let newText = (textView.text as NSString).stringByReplacingCharactersInRange(range, withString: text)
         let numberOfChars = newText.characters.count
-        charCountLabel.text = String(140 - numberOfChars)
+        let labelCount = 140 - numberOfChars
+        if labelCount >= 10 {
+            
+            charCountLabel.textColor = UIColor.whiteColor()
+            
+            if resetFont{
+                charCountLabel.font = UIFont(name:"HelveticaNeue", size: 25.0)
+                UIView.animateWithDuration(0.5, animations: {
+                    self.charCountLabel.transform = CGAffineTransformScale(self.charCountLabel.transform, 0.38554328943, 0.38554328943)
+                })
+                
+                resetFont = false
+            }
+        }
+            
+        else {
+            charCountLabel.textColor = UIColor.redColor()
+            charCountLabel.font = UIFont(name:"HelveticaNeue-Bold", size: 25.0)
+            //charCountLabel.transform = CGAffineTransformScale(charCountLabel.transform, 0.35, 0.35);
+            UIView.animateWithDuration(0.5, animations: {
+                self.charCountLabel.transform = CGAffineTransformScale(self.charCountLabel.transform, 1.1, 1.1)
+            })
+            
+            resetFont = true
+            
+        }
+        
+        charCountLabel.text = String(labelCount)
+        
         return numberOfChars < 140;
     }
 
 
 }
 
+/*
+ func flashBG(){
+ UIView.animateWithDuration(0.7, animations: {
+ self.view.backgroundColor = UIColor.greenColor()
+ 
+ })
+ }
+ */
