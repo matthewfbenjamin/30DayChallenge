78700d906f7d52d932f884e02e24265b2a752f4b
Matthew Benjamin <mbenjamin618@gmail.com>
2016-09-09 17:41:50 -0400

    Finished basic app

---
+++ 19 - TipApp/19 - TipApp/ResultsTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ResultsTableViewCell.swift
+//  19 - TipApp
+//
+//  Created by Matthew Benjamin on 9/8/16.
+//  Copyright © 2016 Matthew Benjamin. All rights reserved.
+//
+
+import UIKit
+
+class ResultsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cellTotalAmountLabel: UILabel!
+    @IBOutlet weak var cellTipAmountLabel: UILabel!
+    @IBOutlet weak var cellPercentLabel: UILabel!
+    
+}
--- 19 - TipApp/19 - TipApp/ViewController.swift
+++ 19 - TipApp/19 - TipApp/ViewController.swift
@@ -8,18 +8,85 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    @IBOutlet weak var perctSliderBar: UISlider!
+    @IBOutlet weak var sliderValLabel: UILabel!
+    @IBOutlet weak var priceTextField: UITextField!
+    var perctTip: Float = 20.0
+    var tipTotal: Float?
+    var finalCost: Float?
+    var billTotal: Float?
+    
+    var newPerctTip: Float = 20.0
+    var percentTipLabel: Int = 20
+    
+    @IBOutlet weak var resultsTableView: UITableView!
+
+    
+    var percentArray:[Float] = []
+    var tipArray:[Float] = []
+    var totalArray: [Float] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func sliderValueChange(sender: UISlider) {
+        perctTip = Float(sender.value)
+        newPerctTip = round(perctTip)
+        percentTipLabel = Int(newPerctTip)
+        
+        sliderValLabel.text = "\(percentTipLabel)"
     }
+    
+    @IBAction func calculateButtonClicked(sender: AnyObject) {
+        percentArray.append(newPerctTip)
+    
+        billTotal = Float(priceTextField.text!)
+        
+        if let bTotal = billTotal {
+            
+            var bTotalNew = round(bTotal*100)/100
+            tipTotal = bTotalNew * newPerctTip/100
+            
+            if let tTotal = tipTotal{
+                var tTotalNew = round(tTotal*100)/100
+                tipArray.append(tTotalNew)
+                finalCost = tTotal + bTotal
+                
+                if let fCost = finalCost {
+                    var fCostNew = round(fCost*100)/100
+                    totalArray.append(fCostNew)
+//                    print(percentArray)
+//                    print(tipArray)
+//                    print(totalArray)
+                    self.resultsTableView.reloadData()
+                }
+            }
+        }
+    }
+    
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return totalArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! ResultsTableViewCell
+        var actualPercent = round(percentArray[indexPath.row])
+        var tipPercent = tipArray[indexPath.row]
+        var totalAmount = totalArray[indexPath.row]
+        
+        cell.cellPercentLabel.text = String("\(actualPercent)%")
+        cell.cellTipAmountLabel.text = String("$\(tipPercent)")
+        cell.cellTotalAmountLabel.text = String("$\(totalAmount)")
+        return cell
+    }
 
 }
 
